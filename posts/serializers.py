abd2193e1ccb7501ff42adb92835c3c1b53fafa9
Azat Yanberdin <azatdjon@yandex.ru>
2023-08-24 17:59:05 +0500

    add new viewsets and routers my project

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth import get_user_model
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,3 +8,10 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
